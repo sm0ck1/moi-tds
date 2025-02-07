3f081c30f08ab5c8fdf4a50f39ace418c4787992
sm0ck1 <seregae86@gmail.com>
2025-02-07 16:47:05 +0100

    actions fix

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -5,9 +5,9 @@ return [
     'connections' => [
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('VITE_PUSHER_APP_KEY'),
-            'secret' => env('VITE_PUSHER_APP_SECRET'),
-            'app_id' => env('VITE_PUSHER_APP_ID'),
+            'key' => env('VITE_PUSHER_APP_KEY', ''),
+            'secret' => env('VITE_PUSHER_APP_SECRET', ''),
+            'app_id' => env('VITE_PUSHER_APP_ID', ''),
             'options' => [
                 'cluster' => env('VITE_PUSHER_APP_CLUSTER', 'eu'),
                 'useTLS'  => true

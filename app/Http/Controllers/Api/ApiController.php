5ecd3f6a27ce7ce1ec2f721b76b44535e3c77e5a
sm0ck1 <seregae86@gmail.com>
2025-02-23 15:26:15 +0100

    Adjust portals

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -15,29 +15,28 @@ class ApiController extends Controller
         $validated = Validator::make($request->all(), [
             'limit' => ['required', 'integer', 'min:1', 'max:100'],
         ]);
-        if($validated->fails()) {
+        if ($validated->fails()) {
             return response()->json(['message' => 'Invalid request.'], 400);
         }
 
         $links = collect();
 
-        DB::transaction(function () use ($request, &$links) {
-            $portalPlacements = PortalPlacement::query()
-                ->select('id', 'external_url')
-                ->where('ping_counter', 0)
-                ->where('get_to_ping', 0)
-                ->limit($request->limit)
-                ->lockForUpdate()
-                ->get();
 
-            if ($portalPlacements->isNotEmpty()) {
-                $links = $portalPlacements->pluck('external_url');
-                $ids = $portalPlacements->pluck('id');
-                PortalPlacement::query()
-                    ->whereIn('id', $ids)
-                    ->update(['get_to_ping' => 1]);
-            }
-        });
+        $portalPlacements = PortalPlacement::query()
+            ->select('id', 'external_url')
+            ->where('ping_counter', 0)
+            ->where('get_to_ping', 0)
+            ->limit($request->limit)
+            ->lockForUpdate()
+            ->get();
+
+        if ($portalPlacements->isNotEmpty()) {
+            $links = $portalPlacements->pluck('external_url');
+            $ids = $portalPlacements->pluck('id');
+            PortalPlacement::query()
+                ->whereIn('id', $ids)
+                ->update(['get_to_ping' => 1]);
+        }
 
         if ($links->isEmpty()) {
             return response()->json(['message' => 'No portal placements found.'], 404);
@@ -49,14 +48,14 @@ class ApiController extends Controller
     public function setSuccessPing(Request $request)
     {
         $validated = Validator::make($request->all(), [
-            'token' => ['required', 'string'],
-            'links' => ['required', 'array'],
+            'token'   => ['required', 'string'],
+            'links'   => ['required', 'array'],
             'links.*' => ['required', 'url'],
         ]);
-        if($validated->fails()) {
+        if ($validated->fails()) {
             return response()->json(['message' => 'Invalid request.'], 400);
         }
-        if($request->token !== env('APP_TOKEN_API')) {
+        if ($request->token !== env('APP_TOKEN_API')) {
             return response()->json(['message' => 'Invalid token.'], 403);
         }
         $links = $request->input('links');
